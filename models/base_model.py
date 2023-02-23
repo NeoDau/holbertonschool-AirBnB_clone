a41c2141d750aca5d71bfb25bf60a3cc069dcf0f
NeoDau <neodau@gmail.com>
2023-02-23 18:00:16 +0000

    base models dict task 4

--- models/base_model.py
+++ models/base_model.py
@@ -24,3 +24,13 @@ class BaseModel:
         newDict["created_at"] = self.created_at.isoformat()
         newDict["updated_at"] = self.updated_at.isoformat()
         return newDict
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
