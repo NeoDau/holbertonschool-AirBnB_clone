92a81e77b0b1144625a2d87c20ca955b6e740c2b
NeoDau <neodau@gmail.com>
2023-02-27 09:18:21 -0800

    base models add storage and datetime

--- models/base_model.py
+++ models/base_model.py
@@ -6,14 +6,23 @@ import models
 
 class BaseModel:
     """class create"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+	self.created_at = datetime.datetime.strptime(
+		kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+	self.updated_at = datetime.datetime.strptime(
+                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
 
     def __str__(self):
-        #[<class name>] (<self.id>) <self.__dict__>
         return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
 
     def save(self):
@@ -26,13 +35,3 @@ class BaseModel:
         newDict["created_at"] = self.created_at.isoformat()
         newDict["updated_at"] = self.updated_at.isoformat()
         return newDict
-
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
