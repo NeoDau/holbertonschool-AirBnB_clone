97462d63030a7ef1985fe9ba7e83510b10fbb197
NeoDau <neodau@gmail.com>
2023-02-27 09:40:56 -0800

    add storage and datatime

--- models/base_model.py
+++ models/base_model.py
@@ -11,18 +11,18 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            self.created_at = datetime.datetime.strptime(
+            self.created_at = datetime.strptime(
                 kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            self.updated_at = datetime.datetime.strptime(
+            self.updated_at = datetime.strptime(
                 kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
-        return (f"[{type(self).__name__}], ({self.id}), {self.__dict__}")
+        return (f"[{self.__class__.__name__}], ({self.id}), {self.__dict__}")
 
     def save(self):
         self.updated_at = datetime.now()
