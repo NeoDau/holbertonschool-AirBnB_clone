9a53a7bb68883f19bab690ce0ebb973ae12fe7f0
NeoDau <neodau@gmail.com>
2023-02-27 21:39:59 -0800

    task 7 add create & show & destroy & all & update

--- console.py
+++ console.py
@@ -28,5 +28,90 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
+    def de_create(self, arg):
+        """create instance"""
+        if not (arg):
+            print("** class name missing **")
+        elif arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            obj = eval[arg]()
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, arg):
+        """priont str representation of a instance"""
+        arg = arg.split()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            try:
+                eval(arg[0])
+                try:
+                    sto = storage.all()
+                    print(sto[f"{strSplit[0]}.{strSplit[1]}"])
+                except Exception:
+                    print("** no istance fund **")
+            except Exception:
+                print("** class doesn't exist **")
+    
+    def do_destroy(self, arg):
+        """del instance based the class"""
+        arg = arg.split()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif len(arg) < 2:
+            print("** instance id missing **")
+        for key, value in storage.all().items():
+            if arg[1] == value.id:
+                del storage.all()[key]
+                storage.save()
+                return
+        print("** no instance fund **")
+
+    def do_all(self, arg):
+        """print atr reptesentation all instance"""
+        arg = arg.split()
+        if len(arg) >= 1:
+            try:
+                eval(arg)()
+                print(storage.all())
+            except Exception:
+                print("** class doesn\'t exist **")
+        else:
+            print(storage.all())
+
+    def do_update(self, arg):
+        """update a istance based on the class name and id"""
+	arg = arg.split()
+
+	if len(arg) == 0:
+	    print("** class name missing **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif len(arg) == 2:
+            print("** attribute name missing **")
+        elif len(arg) == 3:
+            print("** value missing **")
+
+        else:
+
+            try:
+                eval(arg[0])
+
+                try:
+                    for key, value in storage.all().items():
+                        obj = value
+                        if key == f"{string_split[0]}.{string_split[1]}":
+                            setattr(obj, arg[2], arg[3])
+                except Exception:
+                    print("** no instance found **")
+            except Exception:
+		print("** class doesn't exist **")
+ 
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
